9608573684b14a3e7b201fbf928e3040b10a5502
Chaitanya2308 <35360720+Chaitanya2308@users.noreply.github.com>
2021-02-16 15:57:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const fs = require('fs')
 const cors = require('cors')
 const path = require('path')
 
-const port = 8080
+const port = 8000
 
 const app = express()
 app.use(cors())
 
-app.get('/api/products',(req,res)=>{
+
+app.get('/',(req,res)=>
+{
+    res.send("Rest-countries Api using Node")
+})
+app.get('/countries',(req,res)=>{
     const data = fs.readFileSync(path.join(__dirname,"countries_data.json"),'utf8')
     res.set({'Content-Type': 'application/json'})
     res.send(JSON.parse(data))
